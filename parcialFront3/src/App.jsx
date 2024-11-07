3ce0fc0177612fb98740f869909a91a17ffaca88
alvillavel <alvillavel@gmail.com>
2024-11-06 20:14:28 -0500

    Agregue nuave Card

--- parcialFront3/src/App.jsx
+++ parcialFront3/src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-       <h1>Carga de estudiantes</h1>
+       <h1>Carga de estudiantes Final</h1>
        <form></form>
       <Card />
     </div>
